5acb94fa56d27013cda3282f826aba80d52c04c6
shalevamit9 <shalevamit9@gmail.com>
2023-06-09 03:09:47 +0300

    add new line for k8s script

--- k8s-cleanup.sh
+++ k8s-cleanup.sh
@@ -6,6 +6,6 @@ kubectl delete \
 -f k8s/config-map.yaml \
 -f k8s/api-gateway-service.yaml \
 -f k8s/user-service.yaml \
--f k8s/email-service.yaml
+-f k8s/email-service.yaml \
 -f k8s/kafka.yaml \
 -f k8s/zookeeper.yaml
